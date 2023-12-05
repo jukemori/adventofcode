42a18f5e609f341a33a730196a902a75cf0faccb
Jun Ukemori <jukemori@gmail.com>
2023-12-05 23:05:59 +0900

    day 4 part 2

--- day4/day4.js
+++ day4/day4.js
@@ -33,4 +33,56 @@ function part1() {
   return total;
 }
 
+function part2() {
+  let total = 0;
+  let instances = {};
+  for (let line of lines) {
+    let [cardId, data] = line.split(": ");
+    let [winningStr, pickdStr] = data.split("|");
+    let winningNumbers = [];
+    let pickedNumbers = [];
+
+    let match;
+    let pattern = /\d+/g;
+    while ((match = pattern.exec(winningStr)) !== null) {
+      winningNumbers.push(parseInt(match[0]));
+    }
+
+    while ((match = pattern.exec(pickdStr)) !== null) {
+      pickedNumbers.push(parseInt(match[0]));
+    }
+
+    let count = 0;
+    for (let winningNum of winningNumbers) {
+      if (pickedNumbers.includes(winningNum)) {
+        count++;
+      }
+    }
+
+    instances[cardId.replace("Card", "").trim()] = count;
+  }
+
+  function cardCopy(input) {
+    total++;
+    const instancesCount = instances[input];
+
+    if (instancesCount != 0) {
+      for (
+        let i = parseInt(input) + 1;
+        i <= parseInt(input) + instancesCount;
+        i++
+      ) {
+        cardCopy(i);
+      }
+    }
+  }
+
+  for (let key of Object.keys(instances)) {
+    cardCopy(key);
+  }
+
+  return total;
+}
+
 console.log(part1());
+console.log(part2());
