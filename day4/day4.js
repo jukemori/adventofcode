7532911236ee495adce6aa46c24988be76eff797
Jun Ukemori <jukemori@gmail.com>
2023-12-05 22:56:10 +0900

    day4 part1

---
+++ day4/day4.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+let input = fs.readFileSync("./input.txt").toString();
+let lines = input.trim().split("\n");
+
+function part1() {
+  let total = 0;
+  for (let line of lines) {
+    let [cardId, data] = line.split(": ");
+    let [winningStr, pickdStr] = data.split("|");
+    let winningNumbers = [];
+    let pickedNumbers = [];
+
+    let match;
+    let pattern = /\d+/g;
+    while ((match = pattern.exec(winningStr)) !== null) {
+      winningNumbers.push(parseInt(match[0]));
+    }
+
+    while ((match = pattern.exec(pickdStr)) !== null) {
+      pickedNumbers.push(parseInt(match[0]));
+    }
+
+    let count = 0;
+    for (let winningNum of winningNumbers) {
+      if (pickedNumbers.includes(winningNum)) {
+        count++;
+      }
+    }
+
+    total += count != 0 ? Math.pow(2, count - 1) : 0;
+  }
+
+  return total;
+}
+
+console.log(part1());
